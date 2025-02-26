500ab7b846e8a49ed9c07a2ff840f446829bac48
shyam-eranky <veranky@ebay.com>
2025-02-25 19:12:37 -0800

    Removed secret key

--- src/main/java/com/stti/openai/OpenaiApplication.java
+++ src/main/java/com/stti/openai/OpenaiApplication.java
@@ -2,12 +2,12 @@ package com.stti.openai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class OpenaiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OpenaiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiApplication.class, args);
+    }
 }
